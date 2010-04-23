e31ff312a6c9e9ed8a1c058f5e8e0074bc56bba0
Phuong Nguyen <phuongnd08@phuongnd08-laptop.(none)>
2010-04-23 14:11:39 +0700

    Fix: match answer saved properly due to a fix in twig-persist

--- seamoo-persistence/src/test/java/org/seamoo/daos/twigImpl/matching/TwigMatchDaoImplTest.java
+++ seamoo-persistence/src/test/java/org/seamoo/daos/twigImpl/matching/TwigMatchDaoImplTest.java
@@ -10,6 +10,7 @@ import org.seamoo.entities.matching.MatchEvent;
 import org.seamoo.entities.matching.MatchEventType;
 import org.seamoo.entities.question.Question;
 import org.seamoo.persistence.test.LocalDatastoreTest;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -48,5 +49,20 @@ public class TwigMatchDaoImplTest extends LocalDatastoreTest {
 		competitor.addAnswer(new MatchAnswer(MatchAnswerType.IGNORED, "1"));
 		m.addCompetitor(competitor);
 		daoImpl.persist(m);
+	}
+
+	@Test
+	public void matchCompetitorsAndAnswersShouldBePersisted() {
+		Match m = new Match();
+		m.setQuestions(Lists.newArrayList(new Question[] { new Question() }));
+		m.addEvent(new MatchEvent(MatchEventType.ANSWER_QUESTION, new Member(), 1));
+		MatchCompetitor competitor = new MatchCompetitor();
+		competitor.setMember(new Member());
+		competitor.addAnswer(new MatchAnswer(MatchAnswerType.SUBMITTED, "1"));
+		competitor.addAnswer(new MatchAnswer(MatchAnswerType.IGNORED, "1"));
+		m.addCompetitor(competitor);
+		daoImpl.persist(m);
+		Match reloadedM = daoImpl.findByKey(m.getAutoId());
+		Assert.assertEquals(reloadedM.getCompetitors().get(0).getAnswers().size(), 2);
 	}
 }
